07265c75387e3bdef009f7c239d8887540a01862
Pranav Jandu <pranav.jandu@gmail.com>
2021-06-14 14:13:57 +0530

    add category

---
+++ src/admin/helper/adminapicalls.js
@@ -0,0 +1,29 @@
+import { API } from "../../backend";
+
+//category calls
+export const createCategory = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//get all categories
+export const getCategories = () => {
+  return fetch(`${API}/category/all`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
