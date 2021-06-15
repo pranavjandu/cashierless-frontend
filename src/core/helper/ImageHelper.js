68d2044eb677b7177ddc38a95061f339adc9d522
Pranav Jandu <pranav.jandu@gmail.com>
2021-06-15 16:54:22 +0530

    helper function to get image of product

---
+++ src/core/helper/ImageHelper.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import { API } from "../../backend";
+
+const ImageHelper = ({ product }) => {
+  const imageurl = product
+    ? `${API}/product/photo/${product._id}`
+    : `https://images.pexels.com/photos/8333307/pexels-photo-8333307.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260`;
+  return (
+    <div className="rounded border border-success p-2">
+      <img
+        src={imageurl}
+        alt="photo"
+        style={{ maxHeight: "100%", maxWidth: "100%" }}
+        className="mb-3 rounded"
+      />
+    </div>
+  );
+};
+
+export default ImageHelper;
